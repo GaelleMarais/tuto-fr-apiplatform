b2438ab9487a603cd30296baa715825e3c9987ff
Gaelle Marais <marais.gaelle@gmail.com>
2019-07-16 14:25:55 +0200

    Generation de l'entite Organisation

---
+++ organisations_datagouv/src/Entity/Organisation.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\OrganisationRepository")
+ */
+class Organisation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $datagouvid;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $item;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $itemLabel;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatagouvid(): ?string
+    {
+        return $this->datagouvid;
+    }
+
+    public function setDatagouvid(string $datagouvid): self
+    {
+        $this->datagouvid = $datagouvid;
+
+        return $this;
+    }
+
+    public function getItem(): ?string
+    {
+        return $this->item;
+    }
+
+    public function setItem(?string $item): self
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    public function getItemLabel(): ?string
+    {
+        return $this->itemLabel;
+    }
+
+    public function setItemLabel(?string $itemLabel): self
+    {
+        $this->itemLabel = $itemLabel;
+
+        return $this;
+    }
+}
