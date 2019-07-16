4e3d806beff39f5aa0c49a88d6eeecb5dd1c706b
Gaelle Marais <marais.gaelle@gmail.com>
2019-07-16 14:29:10 +0200

    Generation de l'entite Twitter

---
+++ organisations_datagouv/src/Entity/Twitter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TwitterRepository")
+ */
+class Twitter
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $datagouvid;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $twitterUsername;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatagouvid(): ?string
+    {
+        return $this->datagouvid;
+    }
+
+    public function setDatagouvid(string $datagouvid): self
+    {
+        $this->datagouvid = $datagouvid;
+
+        return $this;
+    }
+
+    public function getTwitterUsername(): ?string
+    {
+        return $this->twitterUsername;
+    }
+
+    public function setTwitterUsername(?string $twitterUsername): self
+    {
+        $this->twitterUsername = $twitterUsername;
+
+        return $this;
+    }
+}
