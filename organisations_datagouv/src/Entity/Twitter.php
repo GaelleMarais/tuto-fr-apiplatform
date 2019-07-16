d5b264dcfafe9d258bcdcc21ac3ad09e2e584b49
Gaelle Marais <marais.gaelle@gmail.com>
2019-07-16 15:38:50 +0200

    Twitter.php: Ajout des filtres de recherche

--- organisations_datagouv/src/Entity/Twitter.php
+++ organisations_datagouv/src/Entity/Twitter.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ApiResource(
@@ -12,6 +14,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *     itemOperations={"get"},
  *     normalizationContext={"groups"={"read"}}
  * )
+ * @ApiFilter(SearchFilter::class, properties={"datagouvid": "partial", "twitter":"partial"})
  * @ORM\Entity(repositoryClass="App\Repository\TwitterRepository")
  */
 class Twitter
